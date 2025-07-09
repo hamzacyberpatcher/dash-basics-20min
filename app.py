c649e90071ee011b9317af89d11bea4925187cf1
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-07-09 13:52:47 +0500

    added the table

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 # import the relevant packages
-from dash import Dash, html
+from dash import Dash, html, dash_table
+import pandas as pd
 
 # init the app
 app = Dash()
 
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
 # app layouy
-app.layout = [html.Div(children='Hello World')]
+app.layout = [html.Div(children='Hello World'),
+              dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+              ]
 
 # start running the app
 if __name__ == '__main__':
