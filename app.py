90add3265cff03e83a2bde1362afb0599ceca7ce
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-07-09 13:49:51 +0500

    add comments

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+# import the relevant packages
 from dash import Dash, html
 
+# init the app
 app = Dash()
 
+# app layouy
 app.layout = [html.Div(children='Hello World')]
 
+# start running the app
 if __name__ == '__main__':
     app.run(debug=True)
